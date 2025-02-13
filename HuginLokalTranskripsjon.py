28ade5e6a3729bb1186f6ddce78710b41c2ec6de
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2025-02-13 13:08:38 +0100

    Refactor blob metadata handling and update email sending process; remove unused transcription script

--- HuginLokalTranskripsjon.py
+++ HuginLokalTranskripsjon.py
@@ -18,28 +18,30 @@ client = OpenAI()
 
 AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 AZURE_STORAGE_CONTAINER_NAME = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
-GRAPH_JWT_TOKEN = os.getenv("GRAPH_JWT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") 
 
 filnavn = htl.list_blobs(AZURE_STORAGE_CONNECTION_STRING, AZURE_STORAGE_CONTAINER_NAME)
+metadata = []
 print("\n-----------------------------")
 
 for i in range(len(filnavn)):
     print("\n-----------------------------")
     print(f"Blob {i}: {filnavn[i]}")
-    metadata = htl.get_blob_metadata(AZURE_STORAGE_CONNECTION_STRING, AZURE_STORAGE_CONTAINER_NAME, filnavn[i])
-    print("Sendes: ", metadata['format'], metadata['spraak'])
+    metadata.append(htl.get_blob_metadata(AZURE_STORAGE_CONNECTION_STRING, AZURE_STORAGE_CONTAINER_NAME, filnavn[i]))
+    print("Sendes: ", metadata[i]['format'], metadata[i]['spraak'])
     htl.download_blob(AZURE_STORAGE_CONNECTION_STRING, AZURE_STORAGE_CONTAINER_NAME, filnavn[i], "./blobber/" + filnavn[i])
     htl.delete_blob(AZURE_STORAGE_CONNECTION_STRING, AZURE_STORAGE_CONTAINER_NAME, filnavn[i])
+
+for i in range(len(filnavn)):
     htl.transkriber("./blobber/", filnavn[i])
-    htl.oppsummering("./ferdig_tekst/", filnavn[i], metadata['spraak'] ,metadata['format'])
+    htl.oppsummering("./ferdig_tekst/", filnavn[i], metadata[i]['spraak'] ,metadata[i]['format'])
 
     # Ecode the file to base64
     with open(f"./oppsummeringer/{filnavn[i]}.docx" , "rb") as file:
         base64file = base64.b64encode(file.read()).decode('utf-8')
 
     # Send mail
-    htl.send_email(metadata["upn"], base64file)
+    htl.send_email(metadata[i]["upn"], base64file)
 
     # Delete file from local storage
     os.remove(f"./blobber/{filnavn[i]}")
